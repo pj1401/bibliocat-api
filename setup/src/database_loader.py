f0a69c15174478008a3d7836da3e4e8c1352116c
Patricia Johansson <pj222uc@student.lnu.se>
2026-04-11 14:49:02 +0200

    Update types

--- setup/src/database_loader.py
+++ setup/src/database_loader.py
@@ -2,18 +2,21 @@
 DatabaseLoader class.
 """
 
+from typing import List, Type
 from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from sqlalchemy.exc import SQLAlchemyError
 
+from src.models import Book
+
 
 class DatabaseLoader:
-    def __init__(self, uri: str, base_model: DeclarativeBase):
+    def __init__(self, uri: str, base_model: Type[DeclarativeBase]) -> None:
         self.engine = create_engine(uri, pool_pre_ping=True)
         base_model.metadata.create_all(self.engine)
         self.session_factory = sessionmaker(bind=self.engine)
 
-    def load_table(self, seed_data: list, model: DeclarativeBase):
+    def load_table(self, seed_data: List[Book], model: Type[DeclarativeBase]) -> None:
         """Load seed data into a table."""
         session = self.session_factory()
         try:
