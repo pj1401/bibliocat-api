88c67108a2e9e584bc2189faf5fa1f465d4ed165
Patricia Johansson <pj222uc@student.lnu.se>
2026-04-12 19:43:19 +0200

    Update typing for DatabaseLoader

--- setup/src/database_loader.py
+++ setup/src/database_loader.py
@@ -3,7 +3,7 @@ DatabaseLoader class.
 module: src/database_loader.py
 """
 
-from typing import Type
+from typing import List, Type, TypeVar
 import pandas as pd
 from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
@@ -11,9 +11,12 @@ from sqlalchemy.exc import SQLAlchemyError
 
 from src.models import Author, Publisher
 
+# Use generic type model
+M = TypeVar("M", bound=DeclarativeBase)
+
 
 class DatabaseLoader:
-    def __init__(self, uri: str, base_model: Type[DeclarativeBase]) -> None:
+    def __init__(self, uri: str, base_model: Type[M]) -> None:
         self.engine = create_engine(uri, pool_pre_ping=True)
         base_model.metadata.create_all(self.engine)
         self.session_factory = sessionmaker(bind=self.engine)
@@ -30,7 +33,7 @@ class DatabaseLoader:
         ]
         self.load_table("publishers", publishers, Publisher)
 
-    def load_table(self, table_name: str, data, model: Type[DeclarativeBase]) -> None:
+    def load_table(self, table_name: str, data: List[M], model: Type[M]) -> None:
         """Load seed data from a DataFrame into a table."""
         session = self.session_factory()
         try:
@@ -44,30 +47,3 @@ class DatabaseLoader:
             raise err
         finally:
             session.close()
-
-    def load_books_table(
-        self, seed_data: pd.DataFrame, model: Type[DeclarativeBase]
-    ) -> None:
-        """Load seed data from a DataFrame into a table."""
-        session = self.session_factory()
-        try:
-            if inspect(self.engine).has_table(model.__tablename__):
-                session.query(model).delete()
-
-            model_instances = to_model_instance(seed_data, model)
-
-            session.add_all(model_instances)
-            session.commit()
-            print(
-                f"Successfully loaded {len(model_instances)} records into {model.__name__}."
-            )
-        except SQLAlchemyError as err:
-            session.rollback()
-            raise err
-        finally:
-            session.close()
-
-
-def to_model_instance(data: pd.DataFrame, model: Type[DeclarativeBase]):
-    """Convert DataFrame rows to model instances."""
-    return [model(**row.to_dict()) for _, row in data.iterrows()]
