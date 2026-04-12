dd4d3e29cf4c5276cecff489f1163ce1259dea17
Patricia Johansson <pj222uc@student.lnu.se>
2026-04-12 12:53:19 +0200

    Update models

--- setup/src/models.py
+++ setup/src/models.py
@@ -1,7 +1,15 @@
-from sqlalchemy import Column, Integer, String, Date
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Table, Column, ForeignKey, Integer, String, Date, Numeric
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
+
+
+class Author(Base):
+    __tablename__ = "authors"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    books = relationship("Book", backref="author")
 
 
 class Book(Base):
@@ -9,6 +17,43 @@ class Book(Base):
 
     id = Column(Integer, primary_key=True)
     title = Column(String(255), nullable=False)
-    author = Column(String(255))
     isbn = Column(String(13), unique=True)
     published_date = Column(Date)
+    description = Column(String)
+    language = Column(String)
+    page_count = Column(Integer)
+    rating = Column(Numeric(precision=2, scale=1))
+    voters = Column(Integer)  # Number of reviewers
+
+    publisher_id = Column(Integer, ForeignKey("publishers.id"))
+
+
+authors_books_table = Table(
+    "authors_books_table",
+    Base.metadata,
+    Column(Integer, ForeignKey("authors.id")),
+    Column(Integer, ForeignKey("books.id")),
+)
+
+
+class Publisher(Base):
+    __tablename__ = "publishers"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    books = relationship("Book", backref="publisher")
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+
+
+categories_books_table = Table(
+    "categories_books_table",
+    Base.metadata,
+    Column(Integer, ForeignKey("categories.id")),
+    Column(Integer, ForeignKey("books.id")),
+)
