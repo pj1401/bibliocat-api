7538651e02a7c427d9b50f5d31c7325985c5c08a
Patricia Johansson <pj222uc@student.lnu.se>
2026-04-23 08:47:45 +0200

    Initialise db connection pooling

--- api/main.py
+++ api/main.py
@@ -3,7 +3,11 @@ The starting point of the API.
 module: main.py
 """
 
+from typing import cast
 from flask import Flask
+from src.hooks.database import setup_database_hooks
+from src.config.db_config import DbConfig
+from src.db.connection_manager import DatabaseConnectionManager
 from src.config.load_config import load_config
 from src.blueprints.router import router_bp
 
@@ -12,9 +16,24 @@ def create_app() -> Flask:
     """Set up the application."""
     app = Flask(__name__)
     load_config(app)
+    register_db_manager(app)
     register_blueprints(app)
 
     return app
+
+
+def register_db_manager(app: Flask) -> None:
+    """Register a database manager."""
+    db_manager = DatabaseConnectionManager(
+        DbConfig(
+            cast(str, app.config["DB_HOST"]),
+            cast(str, app.config["DB_NAME"]),
+            cast(str, app.config["DB_USER"]),
+            cast(str, app.config["DB_PASSWORD"]),
+            cast(int, app.config["DB_PORT"]),
+        )
+    )
+    setup_database_hooks(app, db_manager)
 
 
 def register_blueprints(app: Flask) -> None:
--- api/src/db/connection_manager.py
+++ api/src/db/connection_manager.py
@@ -3,14 +3,20 @@ DatabaseConnectionManager class.
 module: src/db/connection_manager.py
 """
 
-from sqlalchemy import PoolProxiedConnection
+from typing import Callable
 import sqlalchemy.pool as pool
+from sqlalchemy import PoolProxiedConnection
+from sqlalchemy.engine.interfaces import DBAPIConnection
 import psycopg2
 from src.config.db_config import DbConfig
 
 
 class DatabaseConnectionManager:
     def __init__(self, db_config: DbConfig):
+        """
+        Construct the connection pool.
+        :see: https://docs.sqlalchemy.org/en/21/core/pooling.html#constructing-a-pool
+        """
         self.dbname = db_config.dbname
         self.host = db_config.host
         self.user = db_config.user
@@ -18,10 +24,13 @@ class DatabaseConnectionManager:
             self._get_connection_creator, max_overflow=10, pool_size=5
         )
 
-    def _get_connection_creator(self):
-        """Get the creator function."""
-        conn = psycopg2.connect(user=self.user, host=self.host, dbname=self.dbname)
-        return conn
+    def _get_connection_creator(self) -> Callable[[], DBAPIConnection]:
+        """Get a callable creator function."""
+
+        def connection_creator() -> psycopg2.extensions.connection:
+            return psycopg2.connect(user=self.user, host=self.host, dbname=self.dbname)
+
+        return connection_creator  # pyright: ignore[reportReturnType]
 
     def get_connection(self) -> PoolProxiedConnection:
         """Get a connection from the pool."""
