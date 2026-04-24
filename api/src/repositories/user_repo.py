ef033c29f1e1c814b649ebcfffc8a44e54f795f2
Patricia Johansson <pj222uc@student.lnu.se>
2026-04-24 15:46:13 +0200

    Add get user by username method

--- api/src/repositories/user_repo.py
+++ api/src/repositories/user_repo.py
@@ -3,7 +3,7 @@ The UserRepository class.
 module: src/repositories/user_repo.py
 """
 
-from sqlalchemy import exc
+from sqlalchemy import exc, select
 from sqlalchemy.orm import Session
 from src.util.errors.error import UniqueViolationError
 from src.util.models.user import User
@@ -15,7 +15,8 @@ class UserRepository:
     def __init__(self, db_manager: DatabaseConnectionManager):
         self.db_manager = db_manager
 
-    def create_user(self, new_user: NewUser):
+    def create_user(self, new_user: NewUser) -> User:
+        """Insert a new user."""
         session: Session | None = None
         try:
             session = self.db_manager.get_session()
@@ -32,6 +33,24 @@ class UserRepository:
             if session is not None:
                 session.rollback()
             raise UniqueViolationError(err)
+        except Exception as err:
+            if session is not None:
+                session.rollback()
+            raise err
+        finally:
+            if session is not None:
+                session.close()
+
+    def get_user_by_username(self, username: str) -> User | None:
+        """Get a user by username."""
+        session: Session | None = None
+        try:
+            session = self.db_manager.get_session()
+            user = session.scalars(
+                select(User).where(User.username == username)
+            ).first()
+            session.commit()
+            return user
         except Exception as err:
             if session is not None:
                 session.rollback()
