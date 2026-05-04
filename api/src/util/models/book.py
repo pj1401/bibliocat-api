91203dcadf4366a74c20932ed7131f75ab821903
Patricia Johansson <pj222uc@student.lnu.se>
2026-05-04 17:57:54 +0200

    Import book model from setup script

---
+++ api/src/util/models/book.py
@@ -0,0 +1,38 @@
+"""
+The Book model.
+module: src/util/models/book.py
+"""
+
+from datetime import datetime, timezone
+from sqlalchemy import (
+    DateTime,
+    Column,
+    ForeignKey,
+    Integer,
+    String,
+    Date,
+    Numeric,
+)
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+class Book(Base):
+    __tablename__ = "books"
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    isbn = Column(String(20), unique=True)
+    published_date = Column(Date)
+    description = Column(String, default="")
+    language = Column(String)
+    page_count = Column(Integer, default=1)
+    rating = Column(Numeric(precision=2, scale=1), default=0)
+    voters = Column(Integer, default=0)  # Number of reviewers
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(
+        DateTime,
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc),
+    )
+    publisher_id = Column(Integer, ForeignKey("publishers.id"))
+    categories = relationship("Category", secondary="categories_books", backref="books")
