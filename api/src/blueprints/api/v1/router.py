d10dd25187b408c7169939f44e7c7403b9c7aa3b
Patricia Johansson <pj222uc@student.lnu.se>
2026-05-07 11:53:54 +0200

    Add link to production

--- api/src/blueprints/api/v1/router.py
+++ api/src/blueprints/api/v1/router.py
@@ -3,10 +3,16 @@ API version 1 router. Registers API endpoints.
 module: src/blueprints/api/v1/router.py
 """
 
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from src.blueprints.api.v1.books.routes import books_bp
 from src.blueprints.api.v1.users.routes import users_bp
 
 router_v1_bp = Blueprint("/", __name__)
 router_v1_bp.register_blueprint(books_bp, url_prefix="/books")
 router_v1_bp.register_blueprint(users_bp, url_prefix="/users")
+
+
+@router_v1_bp.route("/", methods=["GET"])
+def get():
+    response = {"message": "Welcome to version 1 of BiblioCat API!"}
+    return jsonify(response)
--- api/src/blueprints/router.py
+++ api/src/blueprints/router.py
@@ -3,8 +3,17 @@ The main router that registers the api routes.
 module: src/blueprints/router.py
 """
 
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from src.blueprints.api.v1.router import router_v1_bp
 
 router_bp = Blueprint("/", __name__)
 router_bp.register_blueprint(router_v1_bp, url_prefix="/api/v1")
+
+
+@router_bp.route("/", methods=["GET"])
+def get():
+    response = {
+        "message": "Welcome to BiblioCat API!",
+        "version 1": "https://patriciaj.se/bibliocat-api/api/v1",
+    }
+    return jsonify(response)
