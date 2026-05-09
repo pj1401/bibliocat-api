e6ca926e3c2b8843b5e6503310dfc5f709017fc6
Patricia Johansson <pj222uc@student.lnu.se>
2026-05-09 13:29:58 +0200

    Update load_config.py to get secrets from files

--- api/src/config/load_config.py
+++ api/src/config/load_config.py
@@ -25,23 +25,42 @@ class TypedConfig(Config):
     JWT_PUBLIC_KEY: str
     JWT_ALGORITHM: str
 
+def _get_env_or_secret(env_var: str, default: str | int | None = None) -> str | int | None:
+    """
+    Get the value of an environment variable or read it from a file if it ends with _FILE.
+
+    :param env_var: The variable name.
+    :type env_var: str
+    :param default: The default value of the variable.
+    :type default: str | int | None
+    :return: The variable value from the file or environment variable.
+    :rtype: str | int | None
+    """
+    file_var = f"{env_var}_FILE"
+    if file_var in os.environ:
+        # Read from file
+        with open(os.environ[file_var], "r") as f:
+            return f.read().strip()
+    else:
+        # Read from environment variable
+        return os.getenv(env_var, default)
 
 def load_config(app: Flask) -> None:
     """Load the config."""
     app.config.from_mapping(
         {
-            "DB_HOST": os.getenv("POSTGRES_HOST"),
-            "DB_NAME": os.getenv("POSTGRES_DB"),
-            "DB_USER": os.getenv("POSTGRES_USER"),
-            "DB_PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-            "DB_PORT": int(os.getenv("POSTGRES_PORT", "5432")),
-            "SQL_URI": os.getenv("SQL_URI"),
-            "FLASK_DEBUG": os.getenv("FLASK_DEBUG", "True"),
-            "FLASK_HOST": os.getenv("FLASK_HOST", "127.0.0.1"),
-            "FLASK_PORT": os.getenv("FLASK_PORT", 5000),
-            "SECRET_KEY": os.getenv("FLASK_SECRET_KEY"),
-            "JWT_PRIVATE_KEY": os.getenv("JWT_PRIVATE_KEY"),
-            "JWT_PUBLIC_KEY": os.getenv("JWT_PUBLIC_KEY"),
+            "DB_HOST": _get_env_or_secret("POSTGRES_HOST"),
+            "DB_NAME": _get_env_or_secret("POSTGRES_DB"),
+            "DB_USER": _get_env_or_secret("POSTGRES_USER"),
+            "DB_PASSWORD": _get_env_or_secret("POSTGRES_PASSWORD"),
+            "DB_PORT": _get_env_or_secret("POSTGRES_PORT", "5432"),
+            "SQL_URI": _get_env_or_secret("SQL_URI"),
+            "FLASK_DEBUG": _get_env_or_secret("FLASK_DEBUG", "True"),
+            "FLASK_HOST": _get_env_or_secret("FLASK_HOST", "127.0.0.1"),
+            "FLASK_PORT": _get_env_or_secret("FLASK_PORT", 5000),
+            "SECRET_KEY": _get_env_or_secret("FLASK_SECRET_KEY"),
+            "JWT_PRIVATE_KEY": _get_env_or_secret("JWT_PRIVATE_KEY"),
+            "JWT_PUBLIC_KEY": _get_env_or_secret("JWT_PUBLIC_KEY"),
             "JWT_ALGORITHM": "ES512",
         }
     )
