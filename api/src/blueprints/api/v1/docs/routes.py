9ee3fdecb44b717212a5a505f3ae78abd17e86f9
Patricia Johansson <pj222uc@student.lnu.se>
2026-05-14 17:56:43 +0200

    Read PATH_PREFIX from os.environ instead of application context

--- api/src/blueprints/api/v1/docs/routes.py
+++ api/src/blueprints/api/v1/docs/routes.py
@@ -3,12 +3,11 @@ Defines the docs route.
 module: src/blueprints/api/v1/docs/routes.py
 """
 
-from typing import cast
-from flask import current_app
+import os
 from flask_swagger_ui import get_swaggerui_blueprint
 
-path_prefix = cast(str, current_app.config["PATH_PREFIX"])
-SWAGGER_URL = f"{path_prefix}/api/v1/docs"
+PATH_PREFIX = os.environ.get("PATH_PREFIX", "")
+SWAGGER_URL = f"{PATH_PREFIX}/api/v1/docs"
 API_URL = "/static/BiblioCat API v1.openapi.json"
 docs_bp = get_swaggerui_blueprint(
     SWAGGER_URL,
