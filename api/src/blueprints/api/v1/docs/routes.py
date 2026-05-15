c106c38f06221fc424fe8a27fa09eb5f226b9409
Patricia Johansson <pj222uc@student.lnu.se>
2026-05-15 12:07:32 +0200

    Update path to API spec

--- api/src/blueprints/api/v1/docs/routes.py
+++ api/src/blueprints/api/v1/docs/routes.py
@@ -8,7 +8,7 @@ from flask_swagger_ui import get_swaggerui_blueprint
 
 PATH_PREFIX = os.environ.get("PATH_PREFIX", "")
 SWAGGER_URL = f"{PATH_PREFIX}/api/v1/docs"
-API_URL = f"{PATH_PREFIX}/static/Default module.openapi.json"
+API_URL = f"{PATH_PREFIX}/static/default-module.openapi.json"
 docs_bp = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
