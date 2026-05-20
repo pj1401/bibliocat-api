ae22064c4c4e30e7c5ab4a44db4d83ceea4fbd28
Patricia Johansson <pj222uc@student.lnu.se>
2026-05-20 17:31:47 +0200

    Add post methods to reading log controller and repo

--- api/src/controllers/reading_log_controller.py
+++ api/src/controllers/reading_log_controller.py
@@ -3,6 +3,8 @@ The ReadingLogController class.
 module: src/controllers/book_controller.py
 """
 
+from typing import Any
+from src.util.schemas.reading_logs.reading_log_post_params import ReadingLogPostParams
 from src.controllers.base_controller import BaseController
 from src.services.reading_log_service import ReadingLogService
 
@@ -14,3 +16,6 @@ class ReadingLogController(BaseController[ReadingLogService]):
 
     def __init__(self, reading_log_service: ReadingLogService):
         super().__init__(reading_log_service)
+
+    def get_post_arguments(self, data: Any) -> ReadingLogPostParams:
+        return ReadingLogPostParams(**data)
--- api/src/repositories/reading_log_repo.py
+++ api/src/repositories/reading_log_repo.py
@@ -3,6 +3,7 @@ The ReadingLogRepository class.
 module: src/repositories/reading_log_repo.py
 """
 
+from src.util.schemas.reading_logs.new_reading_log import NewReadingLog
 from src.util.filters.reading_log_filters import ReadingLogFilters
 from src.util.models import ReadingLog
 from src.db.connection_manager import DatabaseConnectionManager
@@ -21,3 +22,9 @@ class ReadingLogRepository(BaseRepository[ReadingLog, ReadingLogFilters]):
         base_url: str,
     ):
         super().__init__(db_manager, reading_log_model, base_url)
+
+    def get_new_model(self, arguments: NewReadingLog) -> ReadingLog:
+        return ReadingLog(
+            started_at=arguments.started_at,
+            ended_at=arguments.ended_at,
+        )
