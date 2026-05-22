7289625338effc134e70015f4a1517e9761029df
Patricia Johansson <pj222uc@student.lnu.se>
2026-05-22 07:11:50 +0200

    Add TestGetParams

---
+++ api/tests/test_book_controller.py
@@ -0,0 +1,19 @@
+import pytest
+from unittest.mock import create_autospec
+from src.util.schemas.books.book_query_params import BookQueryParams
+from src.controllers.book_controller import BookController
+from src.services.book_service import BookService
+
+@pytest.fixture
+def mock_service() -> BookService:
+    return create_autospec(BookService)
+
+@pytest.fixture
+def controller(mock_service: BookService):
+    return BookController(book_service=mock_service)
+
+class TestGetParams:
+    def test_returns_book_query_params(self, controller: BookController):
+        params = controller._get_params({"category": "fiction"}) # pyright: ignore[reportPrivateUsage]
+        assert params.category == "fiction"
+        assert isinstance(params, BookQueryParams)
