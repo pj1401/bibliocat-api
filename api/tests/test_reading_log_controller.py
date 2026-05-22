8ee66657d2f5e88d5fff7440f32dcaa03edf2a84
Patricia Johansson <pj222uc@student.lnu.se>
2026-05-22 17:33:15 +0200

    Add tests for ReadingLogController get_validated_arguments

--- api/tests/test_reading_log_controller.py
+++ api/tests/test_reading_log_controller.py
@@ -1,7 +1,10 @@
 import pytest
+from datetime import date
+from pydantic import ValidationError
 from unittest.mock import create_autospec
 from src.controllers.reading_log_controller import ReadingLogController
 from src.services.reading_log_service import ReadingLogService
+from src.util.schemas.reading_logs import ReadingLogParams
 
 
 @pytest.fixture
@@ -16,7 +19,29 @@ def controller(mock_service: ReadingLogService):
 
 class TestGetValidatedArguments:
     def test_returns_reading_log_params(self, controller: ReadingLogController):
-        pass
+        data = {"book_id": "2", "started_at": "2026-01-21", "ended_at": "2026-05-21"}
+        params = controller.get_validated_arguments(data, user_id="42")
+        assert params.user_id == 42
+        assert params.book_id == 2
+        assert params.started_at == date(2026, 1, 21)
+        assert params.ended_at == date(2026, 5, 21)
+        assert isinstance(params, ReadingLogParams)
 
-    def test_raises_validation_error(self, controller: ReadingLogController):
-        pass
+    def test_raises_validation_error_on_missing_field(
+        self, controller: ReadingLogController
+    ):
+        with pytest.raises(ValidationError):
+            controller.get_validated_arguments({"ended_at": "2026-04-17"}, user_id="8")
+
+    def test_raises_validation_error_on_invalid_field(
+        self, controller: ReadingLogController
+    ):
+        with pytest.raises(ValidationError):
+            controller.get_validated_arguments(
+                {
+                    "book_id": "not-int",
+                    "started_at": "2025-11-14",
+                    "ended_at": "2026-04-17",
+                },
+                user_id="8",
+            )
