f8512593a1f3a416d97b3d8f99a002f9cc95bc54
Patricia Johansson <pj222uc@student.lnu.se>
2026-05-22 17:32:25 +0200

    Update get_validated_arguments so it's easier to test

--- api/src/controllers/reading_log_controller.py
+++ api/src/controllers/reading_log_controller.py
@@ -3,7 +3,6 @@ The ReadingLogController class.
 module: src/controllers/book_controller.py
 """
 
-from flask_jwt_extended import get_jwt_identity
 from src.util.schemas.reading_logs.reading_log_params import ReadingLogParams
 from src.controllers.writable_controller import WritableController
 from src.services.reading_log_service import ReadingLogService
@@ -17,8 +16,9 @@ class ReadingLogController(WritableController[ReadingLogService]):
     def __init__(self, reading_log_service: ReadingLogService):
         super().__init__(reading_log_service)
 
-    def get_validated_arguments(self, data: dict[str, str]) -> ReadingLogParams:
-        current_user = get_jwt_identity()
-        data["user_id"] = current_user
+    def get_validated_arguments(
+        self, data: dict[str, str], user_id: str
+    ) -> ReadingLogParams:
+        data["user_id"] = user_id
         # Ignore type error since pydantic validates and coerces the types.
         return ReadingLogParams(**data)  # type: ignore[reportArgumentType]
