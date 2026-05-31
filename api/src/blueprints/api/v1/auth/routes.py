633054a916a0aef41d26e6515d4952ab39f10fea
Patricia Johansson <pj222uc@student.lnu.se>
2026-05-31 16:56:59 +0200

    Move login and register to auth routes

--- api/src/blueprints/api/v1/auth/routes.py
+++ api/src/blueprints/api/v1/auth/routes.py
@@ -1,20 +1,19 @@
 """
-Defines user routes.
-module: src/blueprints/api/v1/users/routes.py
+Defines auth routes.
+module: src/blueprints/api/v1/auth/routes.py
 """
 
 from flask import Blueprint, g
-from src.hooks.auth_required import auth_required
 from src.util.models import User
 from src.util.schemas.user import UserModel
 from src.controllers.user_controller import UserController
 from src.repositories.user_repo import UserRepository
 from src.services.user_service import UserService
 
-users_bp = Blueprint("users", __name__)
+auth_bp = Blueprint("auth", __name__)
 
 
-@users_bp.before_request
+@auth_bp.before_request
 def before_request():
     """Create objects once per request."""
     g.user_repo = UserRepository(g.db_manager, User, g.base_url)
@@ -22,7 +21,11 @@ def before_request():
     g.user_controller = UserController(g.user_service)
 
 
-@users_bp.route("/<int:id>", methods=["DELETE"])
-@auth_required()
-def delete(id: int):
-    return g.user_controller.delete(id)
+@auth_bp.route("/register", methods=["POST"])
+def create_user():
+    return g.user_controller.create_user()
+
+
+@auth_bp.route("/login", methods=["POST"])
+def login_user():
+    return g.user_controller.login()
